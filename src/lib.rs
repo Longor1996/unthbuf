3ffd02f6bf250fda602f34c452c57ecbfe99d14a
Longor1996 <lalongok@gmail.com>
2023-01-06 18:19:55 +0100

    add(fmt): Implement Display for UnthBuf

--- src/lib.rs
+++ src/lib.rs
@@ -276,24 +276,6 @@ impl<const ALIGNED: bool> IntoIterator for UnthBuf<ALIGNED> {
         indices.map(iter)
     }
 }
-
-impl<const ALIGNED: bool> std::fmt::Debug for UnthBuf<ALIGNED> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // let max: Box<dyn std::fmt::Debug> = match ALIGNED {
-        //     true => Box::new(self.aligned_location_of(self.capacity-1)),
-        //     false => Box::new(self.unaligned_location_of(self.capacity-1))
-        // };
-        f.debug_struct("UnthBuf")
-            .field("aligned", &ALIGNED)
-            .field("capacity", &self.capacity)
-            .field("data_len", &self.data.len())
-            .field("bits", &self.bits)
-            .field("mask", &self.mask)
-            //.field("last", &max)
-            .finish()
-    }
-}
-
 impl UnthBuf {
     const BITS_PER_CELL: u8 = usize::BITS as u8;
     
